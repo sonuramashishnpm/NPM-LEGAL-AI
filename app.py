07bf825f827c1e9f2ce20ac7da41bf04968fabf7
Sonu Kumar <sonuramashishnpm@gmail.com>
2026-01-11 19:55:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,5 @@ def upload():
     return jsonify({"path": path})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
